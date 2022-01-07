5c54e38520fbbb63f6d382aa214f18cb2fe94cdc
gams <gabriel.antonioms@gmail.com>
2022-01-06 23:51:03 -0300

    v.6.1 - done smallest sort

--- small_sort.c
+++ small_sort.c
@@ -6,8 +6,14 @@
 /*   By: gantonio <gantonio@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 23:25:16 by gantonio          #+#    #+#             */
-/*   Updated: 2022/01/06 23:32:29 by gantonio         ###   ########.fr       */
+/*   Updated: 2022/01/06 23:50:46 by gantonio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+void	smallest_sort(t_stack *stack, char c)
+{
+	if (stack->number[0] > stack->number[1])
+		swap(stack, c);
+}
